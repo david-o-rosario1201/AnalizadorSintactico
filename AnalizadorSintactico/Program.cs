37703b2ffe9fbcd87ce4d93d498a98a8070163a7
david-o-rosario1201 <finnnn023@gmail.com>
2024-05-28 16:51:49 -0400

    Add project files.

---
+++ AnalizadorSintactico/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Hola mundo");
+
+string patronEnteroValConValor = @"^val _?[A-Za-z0-9]*: Int = [0-9]+$";
+
+while (true)
+{
+    Console.Write("Ingrese un string: ");
+    string valor = Console.ReadLine();
+
+    if (Regex.IsMatch(valor, patronEnteroValConValor))
+    {
+        Console.WriteLine("Todo correcto");
+        Console.WriteLine(valor);
+    }
+    else
+        Console.WriteLine("Incorrecto");
+}
